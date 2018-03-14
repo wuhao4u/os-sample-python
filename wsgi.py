8862f31c8405415e8cd9bdd30410c824aae6b42c
wuhao4u <wuhao4u@gmail.com>
2018-03-14 17:08:28 -0400

    changed greeting

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello From The Other Side!"
 
 if __name__ == "__main__":
     application.run()
